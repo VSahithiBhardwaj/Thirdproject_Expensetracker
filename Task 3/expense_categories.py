aa7551f7dfc906537cca76a05e9031a63be95e0e
VSahithiBhardwaj <142330603+VSahithiBhardwaj@users.noreply.github.com>
2024-06-07 22:01:20 +0530

    Add files via upload

---
+++ Task 3/expense_categories.py
@@ -0,0 +1,9 @@
+categories = ['Food', 'Transportation', 'Entertainment', 'Other']
+
+def get_category():
+    print("Select category:")
+    for i, category in enumerate(categories, 1):
+        print(f"{i}. {category}")
+    choice = int(input("Enter choice: "))
+    return categories[choice - 1]
+
